99b9a16eec1fecb203757a4977ec52593d5483f2
Daniel Gyarmati <40146720210119@litec.ac.at>
2022-10-06 13:37:03 +0200

    EntryControl started

---
+++ 2AHIT-SEW/221006-EntryControl/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _221006_EntryControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
