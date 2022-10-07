fc842564a1f43b7ae61c46cbe8c1c7c8f31a3f2d
Daniel Gyarmati <40146720210119@litec.ac.at>
2022-10-07 07:42:06 +0200

    BMI finished v2

--- 2AHIT-SEW/220929-BMI/Program.cs
+++ 2AHIT-SEW/220929-BMI/Program.cs
@@ -6,9 +6,9 @@ namespace _220929_BMI
     {
         static void Main(string[] args)
         {
-            string input1, input2, input3, gender;
-            double kg, metre, bmi, factor;
-            bool isOk1, isOk2, isOk3;
+            string input1, input2, gender;
+            double mass, height, bmi, factor;
+            bool isOk1, isOk2;
 
             factor = 1;
 
@@ -17,19 +17,21 @@ namespace _220929_BMI
             Console.Write("Geben Sie Ihr Körpergewicht in Kilogramm an: ");
             input1 = Console.ReadLine();
 
-            Console.Write("Geben Sie Ihre Höhe in Meter an: ");
+            Console.Write("Geben Sie Ihre Höhe in cm an: ");
             input2 = Console.ReadLine();
 
             Console.Write("Geben Sie Ihr Geschlecht an (m/w): ");
             gender = Console.ReadLine();
 
-            isOk1 = double.TryParse(input1, out kg);
-            isOk2 = double.TryParse(input2, out metre);
+            isOk1 = double.TryParse(input1, out mass);
+            isOk2 = double.TryParse(input2, out height);
+
+            height = height / 100;
 
             if(isOk1 == true && isOk2 == true)
             {
 
-                bmi = kg / Math.Pow(metre, 2);
+                bmi = mass / Math.Pow(height, 2);
 
                 if(gender.Equals("m"))
                 {
@@ -41,35 +43,35 @@ namespace _220929_BMI
                 }
 
 
-                if(bmi < 16 * factor)
+                if(bmi < 16)
                 {
                    Console.WriteLine("Sie sind stark untergewichtig!");
                 }
-                else if(bmi >= 16 * factor && bmi < 17 * factor)
+                else if(bmi >= 16 && bmi < 17)
                 {
                    Console.WriteLine("Sie sind mäßig untergewichtig!");
                 }
-                else if(bmi >= 17 * factor && bmi < 18.5 * factor)
+                else if(bmi >= 17 && bmi < 18.5)
                 {
                    Console.WriteLine("Sie sind leicht untergewichtig!");
                 }
-                else if(bmi >= 18.5 * factor && bmi < 25 * factor)
+                else if(bmi >= 18.5 && bmi < 25 * factor)
                 {
                    Console.WriteLine("Sie haben ein normales Gewicht!");
                 }
-                else if(bmi >= 25 * factor && bmi < 30 * factor)
+                else if(bmi >= 25 * factor && bmi < 30)
                 {
                    Console.WriteLine("Sie haben Präadipositas!");
                 }
-                else if(bmi >= 30 * factor && bmi < 35 * factor)
+                else if(bmi >= 30 && bmi < 35)
                 {
                    Console.WriteLine("Sie haben Adipositas Grad |!");
                 }
-                else if(bmi >= 35 * factor && bmi < 40 * factor)
+                else if(bmi >= 35 && bmi < 40)
                 {
                    Console.WriteLine("Sie haben Adipositas Grad ||!");
                 }
-                else if(bmi >= 40 * factor)
+                else if(bmi >= 40)
                 {
                    Console.WriteLine("Sie haben Adipositas Grad |||!");
                 }
@@ -77,7 +79,7 @@ namespace _220929_BMI
                 {
                     Console.WriteLine("Fehlerhafte Eingabe!");
                 }
-
+                Console.ReadKey();
             }
         }
     }
