29b7c2b1abc4b2ad0c2acba507622c672ec05063
Daniel Gyarmati <40146720210119@litec.ac.at>
2022-10-07 07:35:21 +0200

    EntryControl finished

--- 2AHIT-SEW/221006-EntryControl/Program.cs
+++ 2AHIT-SEW/221006-EntryControl/Program.cs
@@ -6,7 +6,51 @@ namespace _221006_EntryControl
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string password; //registration
+            string pw;       //log in
+            string username; //registration
+            int wrong = 0;
+            int tries = 3;
+
+            //registration
+            Console.WriteLine("Registrierung!");
+            Console.Write("\n");
+
+            Console.Write("Benutzername: ");
+            username = Console.ReadLine();
+
+            Console.Write("Passwort: ");
+            password = Console.ReadLine();
+
+            Console.Write("\n");
+            do
+            {
+                //log in
+                if(wrong == 0)
+                {
+                    Console.WriteLine("Anmeldung!");
+                }
+                Console.Write("\n");
+
+                Console.Write("Passwort: ");
+                pw = Console.ReadLine();
+
+                if(pw == password)
+                {
+                    Console.WriteLine("Zutritt gewährt!");
+                }
+                else
+                {
+                    wrong += 1;
+                    tries -= 1;
+                    Console.WriteLine("Zutritt Verweigert!");
+                    Console.WriteLine("Anzahl der falschen Versuche: {0}", wrong);
+                    Console.WriteLine("Zahl der noch verbleibenden Versuche: {0}", tries);
+                }
+                   
+            } while (wrong < 3 && pw != password);
+
+            Console.ReadKey();
         }
     }
 }
