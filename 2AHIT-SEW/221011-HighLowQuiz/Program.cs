3596913de97c8a7f8d1c0155a8fe2dedb929d981
Daniel Gyarmati <40146720210119@litec.ac.at>
2022-10-13 12:25:46 +0200

    HighLowQuiz finished

---
+++ 2AHIT-SEW/221011-HighLowQuiz/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace _221011_HighLowQuiz
+{
+    class Program
+    {
+        public static int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(1, 100);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HighLow-Ratespiel!");
+
+            string input1;
+            double rand, tries = 0, input2;
+            bool isOk;
+
+            rand = RandomNumber(1, 100);
+
+            Console.Write("Geben Sie eine Zahlen von 1-100 ein: ");
+
+            do
+            {
+                input1 = Console.ReadLine();
+
+                isOk = double.TryParse(input1, out input2);
+
+                if(isOk == true && input2 <= 100 && input2 >= 1)
+                {
+                    if(input2 < rand)
+                    {
+                        Console.WriteLine("Zu klein!");
+                    }
+                    else if(input2 > rand)
+                    {
+                        Console.WriteLine("Zu Groß!");
+                    }
+                    else if(input2 == rand)
+                    {
+                        Console.WriteLine("Richtig!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Fehlerhafte Eingabe!");
+
+                    Console.Write("Geben Sie eine Zahlen von 1-100 ein: ");
+                }
+
+                tries++;
+
+            } while (input2 != rand);
+
+            Console.WriteLine("Versuche insgesamt: {0}", tries);
+
+            Console.ReadKey();
+        }
+    }
+}
