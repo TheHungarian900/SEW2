52966fcea050deb130fb9891a8a435689d6ac823
Daniel Gyarmati <40146720210119@litec.ac.at>
2022-10-26 10:51:15 +0200

    Change (Ü8) finished

---
+++ 2AHIT-SEW/221020-Change/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _221020_Change
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExchange_Click(object sender, RoutedEventArgs e)
+        {
+            string change;
+
+            change = txtbox1.Text;
+            
+            txtbox1.Text = txtbox2.Text;
+
+            txtbox2.Text = change;
+
+          
+        }
+    }
+}
