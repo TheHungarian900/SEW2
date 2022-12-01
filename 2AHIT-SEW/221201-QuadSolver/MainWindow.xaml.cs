1c9ef517b35fadee2074dc204b830dc83c984c16
Daniel Gyarmati <40146720210119@litec.ac.at>
2022-12-01 13:40:10 +0100

    feat: QuadSolver finished

---
+++ 2AHIT-SEW/221201-QuadSolver/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _221201_QuadSolver
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double a, b, c;
+
+            if(double.TryParse(txtA.Text, out a) &&
+               double.TryParse(txtB.Text, out b) &&
+               double.TryParse(txtC.Text, out c))
+            {
+                lblResult.Content = QuadSolver.Evaluate(a, b, c);
+            }
+            else
+            {
+                lblResult.Content = "Bitte Zahlen eingeben!";
+            }
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ 2AHIT-SEW/221201-QuadSolver/QuadSolver.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _221201_QuadSolver
+{
+    class QuadSolver
+    {
+        public static string Evaluate(double a, double b, double c)
+        {
+            double D;        //Diskriminante
+            double x1, x2;   //Solutions
+
+            if(a == 0)
+            {
+                if (b != 0)
+                {
+                    x1 = -c / b;
+                    return "Eine reelle Lösung: " + x1;
+                }
+                else
+                {
+                    return "Blödsinn!";
+                }
+            }
+            else if(b == 0)
+            {
+                double x1Squared = -c / a;
+
+                if (x1Squared < 0)
+                {
+                    return "Komplex";
+                }
+                else
+                {
+                    x1 = Math.Sqrt(x1Squared);
+                    x2 = -x1;
+
+                    return "Zwei reelle Lösungen x1 = " + x1 + " x2 = " + x2;
+                }
+            }
+
+            D = Math.Pow(b, 2) - 4 * a * c;
+
+            if(D == 0)
+            {
+                return "Eine Lösung: " + (-b / (2 * a));
+            }
+            else if(D > 0)
+            {
+                x1 = (-b - Math.Sqrt(D)) / (2 * a);
+                x2 = (-b + Math.Sqrt(D)) / (2 * a);
+
+                return "Zwei reelle Lösungen x1 = " + x1 + " x2 = " + x2;
+            }
+            else
+            {
+                return "Komplex";
+            }
+        }
+    }
+}
