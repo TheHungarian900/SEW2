f4db9045d8839d04b1553d817fcb7fb767c1a445
Daniel Gyarmati <40146720210119@litec.ac.at>
2023-02-09 14:24:26 +0100

    feat: ex 22 started & recursion finished

---
+++ 2AHIT-SEW/230209-Ex22-CalculatorWPF/MathOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nsMath
+{
+    class MathOperations
+    {
+        public const double PI = 3.1415926;
+        public const double E = 2.718281828;
+
+        public static double Add(double a, double b) { return a + b; }
+        public static double Sub(double a, double b) { return a - b; }
+        public static double Mult(double a, double b) { return a * b; }
+        public static double Div(double a, double b)
+        {
+            if(b == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return a / b;
+        }
+
+        public static double Abs(double a) { return Math.Abs(a); }
+        public static int Signum(double a)
+        {
+            if (a == 0) return 0;
+            return (a < 0) ? -1 : 1;
+        }
+    }
+}
---
+++ 2AHIT-SEW/230209-Recursion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _230209_Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sum of 10 to 0: " + Sum(10));
+
+            Console.WriteLine("Sum as recursion");
+        }
+
+        //Recursion Example - Sum from i to 0
+        public static int Sum(int i)
+        {
+            if (i == 0)
+                return 0;
+            else
+                return i + Sum(i - 1);
+        }
+    }
+}
